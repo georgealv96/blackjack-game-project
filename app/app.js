40ebbd3e6c77fb1d390518f4ed6c6ecc88dfaa21
georgealv96 <george1996alvarado@gmail.com>
2023-06-20 11:53:36 -0600

    rearrenge functions, change functionalities

--- app/app.js
+++ app/app.js
@@ -20,13 +20,13 @@ const originalDeck = buildOriginalDeck()
 /*----- app's state (variables) -----*/
 
 let availableAmount = 1000
-let betAmount
-let cardCount
+let betAmount = 0
+let cardCount = 0
 let insuranceAmount
 let tookInsurance
 let aceCount
-let playersCardsSum = 0
-let dealersCardsSum = 0
+let playersCardsSum
+let dealersCardsSum
 let shuffledDeck
 let playersCards
 let dealersCards
@@ -41,8 +41,6 @@ const messageEl = document.createElement('div')
 const secondPageEl = document.getElementById('second-page')
 const playerSideEl = document.getElementById('player-side')
 const dealerSideEl = document.getElementById('dealer-side')
-const hitBtnEl = document.getElementById('hit-btn')
-const standBtnEl = document.getElementById('stand-btn')
 const dealerHasEl = document.getElementById('dealer-bet-info')
 const playerHasEl = document.getElementById('player-bet-info')
 const doubleDownBtn = document.createElement('button')
@@ -53,20 +51,8 @@ const bottomSideEl = document.getElementById('second-page-buttons')
 
 chipsEl.addEventListener('click', handleChips)
 placeBetBtn.addEventListener('click', placeBet)
-hitBtnEl.addEventListener('click', handleHitBtn)
-standBtnEl.addEventListener('click', function () {
-  dealersTurn()
-  compareResults()
-})
 
 /*----- functions -----*/
-init()
-
-// When the page initializes...
-function init() {
-  betAmount = 0
-  cardCount = 0
-}
 
 // When player selects amount of dollars to bet...
 function handleChips(evt) {
@@ -113,9 +99,9 @@ function handleChips(evt) {
     default:
       return
   }
-
-  // Update available and bet amounts
+  // Update available amount to bet
   availableAmountEl.innerHTML = `AVAILABLE: <br><span> $</span>${availableAmount}`
+  // Update bet amounts
   betAmountEl.innerHTML = `BET: <br><span> $</span>${betAmount}`
 }
 
@@ -125,6 +111,10 @@ function placeBet(evt) {
   if (betAmount > 0) {
     secondPageEl.style.opacity = '1'
     renderGame()
+  } else if (betAmount === 0 && availableAmount === 0) {
+    evt.preventDefault()
+    makeYourBetMsg(`Insufficient funds. Reset page.`)
+    return
   } else {
     evt.preventDefault()
     makeYourBetMsg(`Don't be cheap. Make a bet!`)
@@ -149,6 +139,15 @@ function makeYourBetMsg(message) {
 
 // When the game starts and is displayed in the screen...
 function renderGame() {
+  bottomSideEl.innerHTML = `<button id="hit-btn">HIT</button>
+  <button id="stand-btn">STAND</button>`
+  const hitBtnEl = document.getElementById('hit-btn')
+  const standBtnEl = document.getElementById('stand-btn')
+  hitBtnEl.addEventListener('click', handleHitBtn)
+  standBtnEl.addEventListener('click', function () {
+    dealersTurn()
+    compareResults()
+  })
   // This makes the second page display
   secondPageEl.style.zIndex = '1'
   secondPageEl.style.display = 'grid'
@@ -199,181 +198,190 @@ function renderGame() {
   }
 
   // GAME CONTINUES HERE...
-}
+  // When the table is being displayed...
+  function renderTable() {
+    hitBtnEl.disabled = false
+    standBtnEl.disabled = false
+    // Reset both dealer's and player's hands
+    dealersCards = []
+    playersCards = []
+    // Reset both dealer's and player's cards sum
+    playersCardsSum = 0
+    dealersCardsSum = 0
+    // Reset insurance status
+    tookInsurance = false
+    for (let i = 0; i < 4; i++) {
+      if (i % 2 === 0) {
+        playersCards.push(shuffledDeck.shift())
+      } else {
+        dealersCards.unshift(shuffledDeck.shift())
+      }
+    }
 
-// When the table is being displayed...
-function renderTable() {
-  // Reset both dealer's and player's hands
-  dealersCards = []
-  playersCards = []
-  // Reset insurance status
-  tookInsurance = false
-  for (let i = 0; i < 4; i++) {
-    if (i % 2 === 0) {
-      playersCards.push(shuffledDeck.shift())
+    // Sum the player's first two cards, and the dealer's first two cards
+    for (let i = 0; i < 2; i++) {
+      playersCardsSum += playersCards[i].value
+      dealersCardsSum += dealersCards[i].value
+    }
+
+    // If the sum of the player's cards or the sum of the dealer's equal 22 then substract 10
+    playersCardsSum = checkForSumOf22(playersCardsSum)
+    dealersCardsSum = checkForSumOf22(dealersCardsSum)
+
+    renderDeckInContainer(playersCards, playerSideEl, 0)
+    renderDeckInContainer(dealersCards, dealerSideEl, 1)
+    ///////////////////////////////
+    console.log(playersCards) //
+    console.log(dealersCards) //
+    console.log(playersCardsSum) //
+    ///////////////////////////////
+    // Update bet-info
+    playerHasEl.innerHTML = `<span id="arrow-2">></span> PLAYER has ${playersCardsSum}`
+    dealerHasEl.innerHTML = `<span id="arrow-3">></span> DEALER has ${
+      dealersCardsSum - dealersCards[0].value
+    }`
+    // Check if the player has a blackjack
+    if (playersCardsSum === 21 && dealersCardsSum !== 21) {
+      console.log('Player WINS!') // CHANGE THIS
+      dealersTurn()
+      console.log(betAmount + '  ' + availableAmount)
+      availableAmount += betAmount * 2.5
+      renderResults('BLACKJACK!')
+    }
+    // Update current bet information on the screen
+    updateBetInfo()
+  }
+
+  // Check for a sum of 22 (two Aces)
+  function checkForSumOf22(sum) {
+    if (sum === 22) {
+      return sum - 10
     } else {
-      dealersCards.unshift(shuffledDeck.shift())
+      return sum
     }
   }
 
-  // Sum the player's first two cards, and the dealer's first two cards
-  for (let i = 0; i < 2; i++) {
-    playersCardsSum += playersCards[i].value
-    dealersCardsSum += dealersCards[i].value
+  // Search for an Ace in a playing hand
+  function searchForAce(cardHand) {
+    let quantity = 0
+    cardHand.forEach(function (card) {
+      if (card.isAce) quantity += card.isAce
+    })
+    return quantity
   }
 
-  // If the sum of the player's cards or the sum of the dealer's equal 22 then substract 10
-  playersCardsSum = checkForSumOf22(playersCardsSum)
-  dealersCardsSum = checkForSumOf22(dealersCardsSum)
-
-  renderDeckInContainer(playersCards, playerSideEl, 0)
-  renderDeckInContainer(dealersCards, dealerSideEl, 1)
-  ///////////////////////////////
-  console.log(playersCards) //
-  console.log(dealersCards) //
-  console.log(playersCardsSum) //
-  ///////////////////////////////
-  // Update bet-info
-  playerHasEl.innerHTML = `<span id="arrow-2">></span> PLAYER has ${playersCardsSum}`
-  dealerHasEl.innerHTML = `<span id="arrow-3">></span> DEALER has ${
-    dealersCardsSum - dealersCards[0].value
-  }`
-  // Check if the player has a blackjack
-  if (playersCardsSum === 21 && dealersCardsSum !== 21) {
-    console.log('Player WINS!') // CHANGE THIS
-    dealersTurn()
-    console.log(betAmount + '  ' + availableAmount)
-    availableAmount += betAmount * 2.5
-    renderResults('BLACKJACK!')
+  // When player hits...
+  function handleHitBtn(evt) {
+    // Remove the 'insurance' and 'double' buttons
+    insuranceBtn.remove()
+    doubleDownBtn.remove()
+    // Let the player keep clicking on the "Hit" button while the sum of their cards is less than 22
+    if (playersCardsSum < 22) {
+      getAnExtraCard()
+    } else {
+      evt.preventDefault()
+    }
+    // Check again for the sum of the player's cards and if it's greater or equal than 22 then it's the dealer's turn now
+    if (playersCardsSum >= 22) {
+      dealersTurn()
+      compareResults()
+    }
   }
-  // Update current bet information on the screen
-  updateBetInfo()
-}
 
-// Check for a sum of 22 (two Aces)
-function checkForSumOf22(sum) {
-  if (sum === 22) {
-    return sum - 10
-  } else {
-    return sum
+  // Update the bet information on the table
+  function updateBetInfo() {
+    document.getElementById(
+      'your-bet-info'
+    ).innerHTML = `<span id="arrow-1">></span> YOUR BET: $${betAmount}`
+    document.getElementById(
+      'available-bet-info'
+    ).innerHTML = `<span id="arrow-4">></span> AVAILABLE: $${availableAmount}`
   }
-}
 
-// Search for an Ace in a playing hand
-function searchForAce(cardHand) {
-  let quantity = 0
-  cardHand.forEach(function (card) {
-    if (card.isAce) quantity += card.isAce
-  })
-  return quantity
-}
-
-// When player hits...
-function handleHitBtn(evt) {
-  // Remove the 'insurance' and 'double' buttons
-  insuranceBtn.remove()
-  doubleDownBtn.remove()
-  // Let the player keep clicking on the "Hit" button while the sum of their cards is less than 22
-  if (playersCardsSum < 22) {
-    getAnExtraCard()
-  } else {
-    evt.preventDefault()
+  // Get the player an extra card
+  function getAnExtraCard() {
+    playersCards.push(shuffledDeck.shift())
+    playersCardsSum += playersCards[playersCards.length - 1].value
+    playerSideEl.innerHTML += `<div class="card ${
+      playersCards[playersCards.length - 1].face
+    }"></div>`
+    playerHasEl.innerHTML = `<span id="arrow-2">></span> PLAYER has ${playersCardsSum}`
   }
-  // Check again for the sum of the player's cards and if it's greater or equal than 22 then it's the dealer's turn now
-  if (playersCardsSum >= 22) {
-    dealersTurn()
-    compareResults()
-  }
-}
 
-// Update the bet information on the table
-function updateBetInfo() {
-  document.getElementById(
-    'your-bet-info'
-  ).innerHTML = `<span id="arrow-1">></span> YOUR BET: $${betAmount}`
-  document.getElementById(
-    'available-bet-info'
-  ).innerHTML = `<span id="arrow-4">></span> AVAILABLE: $${availableAmount}`
-}
-
-// Get the player an extra card
-function getAnExtraCard() {
-  playersCards.push(shuffledDeck.shift())
-  playersCardsSum += playersCards[playersCards.length - 1].value
-  playerSideEl.innerHTML += `<div class="card ${
-    playersCards[playersCards.length - 1].face
-  }"></div>`
-  playerHasEl.innerHTML = `<span id="arrow-2">></span> PLAYER has ${playersCardsSum}`
-}
-
-// Show the dealer's second card
-function dealersTurn() {
-  dealerSideEl.innerHTML = ''
-  dealersCards.forEach(function (card) {
-    dealerSideEl.innerHTML += `<div class="card ${card.face}"></div>`
-  })
-  dealerHasEl.innerHTML = `<span id="arrow-2">></span> DEALER has ${dealersCardsSum}`
-  while (dealersCardsSum < 17) {
-    dealersCards.unshift(shuffledDeck.shift())
-    dealerSideEl.innerHTML += `<div class="card ${dealersCards[0].face}"></div>`
-    dealersCardsSum += dealersCards[0].value
+  // Show the dealer's second card
+  function dealersTurn() {
+    dealerSideEl.innerHTML = ''
+    dealersCards.forEach(function (card) {
+      dealerSideEl.innerHTML += `<div class="card ${card.face}"></div>`
+    })
     dealerHasEl.innerHTML = `<span id="arrow-2">></span> DEALER has ${dealersCardsSum}`
-    console.log(dealersCardsSum)
-    console.log(dealersCards)
-  }
-  standBtnEl.disabled = true
-  hitBtnEl.disabled = true
-}
-
-// When the results are being compared
-function compareResults() {
-  if (
-    playersCardsSum < 22 &&
-    (playersCardsSum > dealersCardsSum || dealersCardsSum >= 22)
-  ) {
-    console.log('Player WINS!') // CHANGE THIS
-    availableAmount += betAmount * 2
-    renderResults('PLAYER WINS!')
-  } else if (playersCardsSum > 21 || dealersCardsSum > playersCardsSum) {
-    console.log('Dealer WINS!') // CHANGE THIS
-    renderResults('DEALER WINS!')
-  } else {
-    console.log("It's a PUSH!") // CHANGE THIS
-    availableAmount += betAmount
-    renderResults("IT'S A PUSH!")
-  }
-  updateBetInfo()
-}
-
-// Render results and a 'bet again' button
-function renderResults(message) {
-  bottomSideEl.innerHTML = `<h1>${message}</h1>
-  <button id="go-back">BET AGAIN</button>`
-  const goBackBtn = document.getElementById('go-back')
-  goBackBtn.addEventListener('click', function (evt) {
-    handleChips(evt)
-    secondPageEl.style.opacity = '0'
-    secondPageEl.style.zIndex = '-1'
-  })
-}
-//
-function renderDeckInContainer(deck, container, faceDown) {
-  container.innerHTML = ''
-  // Let's build the cards as a string of HTML
-  let cardsHtml = ''
-  deck.forEach(function (card, idx) {
-    // If this is the dealer's second card then face it down
-    if (idx === 0 && faceDown) {
-      cardsHtml += `<div class="card ${card.face} ${card.back} face-down"></div>`
-    } else {
-      cardsHtml += `<div class="card ${card.face}"></div>`
+    while (dealersCardsSum < 17) {
+      dealersCards.unshift(shuffledDeck.shift())
+      dealerSideEl.innerHTML += `<div class="card ${dealersCards[0].face}"></div>`
+      dealersCardsSum += dealersCards[0].value
+      dealerHasEl.innerHTML = `<span id="arrow-2">></span> DEALER has ${dealersCardsSum}`
+      console.log(dealersCardsSum)
+      console.log(dealersCards)
     }
-  })
-  // Or, use reduce to 'reduce' the array into a single thing - in this case a string of HTML markup
-  // const cardsHtml = deck.reduce(function(html, card) {
-  //   return html + `<div class="card ${card.face}"></div>`;
-  // }, '');
-  container.innerHTML = cardsHtml
+    standBtnEl.disabled = true
+    hitBtnEl.disabled = true
+  }
+
+  // When the results are being compared
+  function compareResults() {
+    if (
+      playersCardsSum < 22 &&
+      (playersCardsSum > dealersCardsSum || dealersCardsSum >= 22)
+    ) {
+      console.log('Player WINS!') // CHANGE THIS
+      availableAmount += betAmount * 2
+      renderResults('PLAYER WINS!')
+    } else if (playersCardsSum > 21 || dealersCardsSum > playersCardsSum) {
+      console.log('Dealer WINS!') // CHANGE THIS
+      renderResults('DEALER WINS!')
+    } else {
+      console.log("It's a PUSH!") // CHANGE THIS
+      availableAmount += betAmount
+      renderResults("IT'S A PUSH!")
+    }
+    updateBetInfo()
+  }
+
+  // Render results and a 'bet again' button
+  function renderResults(message) {
+    bottomSideEl.innerHTML = `<h1>${message}</h1>
+  <button id="go-back">BET AGAIN</button>`
+    const goBackBtn = document.getElementById('go-back')
+    goBackBtn.addEventListener('click', function (evt) {
+      secondPageEl.style.opacity = '0'
+      secondPageEl.style.zIndex = '-1'
+      betAmount = 0
+      // Update bet amounts
+      betAmountEl.innerHTML = `BET: <br><span> $</span>${betAmount}`
+      // Update available amount to bet
+      availableAmountEl.innerHTML = `AVAILABLE: <br><span> $</span>${availableAmount}`
+      handleChips(evt)
+    })
+  }
+  //
+  function renderDeckInContainer(deck, container, faceDown) {
+    container.innerHTML = ''
+    // Let's build the cards as a string of HTML
+    let cardsHtml = ''
+    deck.forEach(function (card, idx) {
+      // If this is the dealer's second card then face it down
+      if (idx === 0 && faceDown) {
+        cardsHtml += `<div class="card ${card.face} ${card.back} face-down"></div>`
+      } else {
+        cardsHtml += `<div class="card ${card.face}"></div>`
+      }
+    })
+    // Or, use reduce to 'reduce' the array into a single thing - in this case a string of HTML markup
+    // const cardsHtml = deck.reduce(function(html, card) {
+    //   return html + `<div class="card ${card.face}"></div>`;
+    // }, '');
+    container.innerHTML = cardsHtml
+  }
 }
 
 // When a shuffled deck is needed... (#)
